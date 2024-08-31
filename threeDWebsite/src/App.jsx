4218fdf8e57586030bdc8077c9078e436071dbdf
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-08-31 11:09:46 +0530

    create a 3d cilinder components

---
+++ threeDWebsite/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import Scene from './Scene'
+import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing'
+
+
+export default function App() {
+
+  return (
+    <>
+      <Canvas flat camera={{ fov: 35 }}>
+        <OrbitControls />
+        <ambientLight />
+        <Scene />
+        <EffectComposer>
+          <Bloom
+            mipmapBlur
+            intensity={2.0} // The bloom intensity.
+            luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+            luminanceSmoothing={0}
+          />
+        </EffectComposer>
+      </Canvas>
+      <div className="w-fll my-4 bg-black">
+        <h1 className="text-white">Welcome to Three Js</h1>
+      </div>
+    </>
+  )
+}
